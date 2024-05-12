90a6da1ddbd0f46b478ee45bbe881a9b6a82fda6
kodi7rd <KodiRealDebridIsrael@github.com>
2024-05-13 02:51:41 +0300

    0.6.20a | service.py | MDBlist Rating | Add 10 API Keys | random: usage if "use_my_custom_list" is configured

--- resources/lib/service.py
+++ resources/lib/service.py
@@ -46,6 +46,25 @@ class RatingsService(xbmc.Monitor):
                 self.waitForAbort(15)
                 continue
             api_key = self.get_infolabel("Skin.String(mdblist_api_key)")
+
+            # KODI-RD-IL
+            if api_key == 'use_my_custom_list':
+                MDBLIST_API_KEYS = {
+                    "darksubsil1-api_key": "csq5pq5vhf4r1snagff7qukcu",
+                    "darksubsil2-api_key": "m1azvroj6xgt6x05urkmd9m9u",
+                    "darksubsil3-api_key": "dbm160enk1t0shknn3r2bh003",
+                    "darksubsil4-api_key": "n5p3p7mvdb9tmdq49mrp59tdu",
+                    "darksubsil5-api_key": "5608d5y1dkhogghe4ly4nkjr9",
+                    "darksubsil6-api_key": "blgzz5pfmfb973mgy1lp4n3s8",
+                    "darksubsil7-api_key": "9wcsvxj6gjv7io6p9pcqfeqbw",
+                    "darksubsil8-api_key": "udrqs9ni2gdf6qvtnytnodrxt",
+                    "darksubsil9-api_key": "j49h9i7aohe7g4pdpl4o5rp38",
+                    "darksubsil10-api_key": "kq4ur9et7hceu2h0e00l2hw0j"
+                }
+                import random
+                api_key = random.choice(list(MDBLIST_API_KEYS.values()))
+            ############
+
             if not api_key:
                 self.waitForAbort(10)
                 continue
